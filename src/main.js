d4db587393a492276ee52897c5aa20c9862ac79d
DanyalBani <mr.d.bani.d@gmail.com>
2022-02-08 08:00:28 +0330

    inti and base mouse moving and creating rectangles created

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from "vue";
+import VueKonva from "vue-konva";
 
-Vue.config.productionTip = false
+import App from "./App.vue";
+import router from "./router";
+
+Vue.config.productionTip = false;
+Vue.use(VueKonva);
 
 new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+    router,
+    render: (h) => h(App),
+}).$mount("#app");
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,81 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <v-stage
+        ref="stage"
+        @click="handleStageClick"
+        @mousemove="handleStageMove"
+        :config="configKonva"
+    >
+        <v-layer>
+            <v-rect
+                :key="rect.id"
+                v-for="rect in rectangles"
+                :config="rect"
+            ></v-rect>
+        </v-layer>
+    </v-stage>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+const width = window.innerWidth;
+const height = window.innerHeight;
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+    name: "Home",
+    data() {
+        return {
+            clickCounter: 0,
+            configKonva: {
+                width: width,
+                height: height,
+            },
+            rectangles: [
+                {
+                    x: -1300,
+                    y: 940,
+                    width: 100,
+                    height: 100,
+                    fill: "black",
+                    stroke: "red",
+                    strokeWidth: 1,
+                },
+            ],
+        };
+    },
+    methods: {
+        setRect(obj) {
+            this.rectangles.push(obj);
+        },
+        handleStageClick() {
+            const mousePosition = this.$refs.stage
+                .getNode()
+                .getPointerPosition();
+            if (this.clickCounter == 0) {
+                console.log(mousePosition);
+                ++this.clickCounter;
+                const newRect = {
+                    x: mousePosition.x,
+                    y: mousePosition.y,
+                    width: 0,
+                    height: 0,
+                    stroke: "blue",
+                    strokeWidth: 1,
+                };
+                this.setRect(newRect);
+            }
+        },
+        handleStageMove() {
+            if (this.clickCounter == 1) {
+                const mousePosition = this.$refs.stage
+                    .getNode()
+                    .getPointerPosition();
+
+                this.rectangles.at(-1).width =
+                    mousePosition.x - this.rectangles.at(-1).x;
+                this.rectangles.at(-1).height =
+                    mousePosition.y - this.rectangles.at(-1).y;
+                console.log(this.rectangles.at(-1));
+            }
+        },
+    },
+};
 </script>
